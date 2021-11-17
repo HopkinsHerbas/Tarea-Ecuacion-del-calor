813f4b410d75009d29ee57b1ffdab0ff5687d28c
jhismaci <61795813+jhismaci@users.noreply.github.com>
2021-11-16 23:38:23 -0500

    Update heatimplicito.c

--- heatimplicito.c
+++ heatimplicito.c
@@ -1,82 +1,91 @@
 #include "tools.h"
 
-//Implicit
-
-
 double funcion(double x, double t);
 
 
+
+
 int main(){
-	double h = 0.01, k=0.00005; //k es deltaT y h es deltaX
+	double h = 0.01, k=0.0005; //k es deltaT y h es deltaX
 	double lambda = k/h*h;
-	double *x,*t,*b,*frontera;
+	double *x,*t,*inicial;
 	int i,j;
 	int tmax=1;
-	int n,m=10;
+	int n=10,m=10; // particion espacial vs temporal
 	double tolerancia = 0.001;
 	
-	
-	b = (double *) malloc(n * sizeof(double));
-	frontera = (double *) malloc(n+1 * sizeof(double));
+
+	inicial = (double *) malloc(n * sizeof(double));
 	
 	
 	//Vector de posición x (discretización espacial)
 	x = (double *) malloc( n * sizeof(double) );
 
 	x[0] = 0;
-	x[n] = 1;
-	for( i = 1; i < n; i++ ) {
+	x[n-1] = 1;
+	for( i = 1; i < n-1; i++ ){
       	x[i] = x[i-1] + h ;
+		printf("%f",x[i]);
 	}
 
 	//Vector de tiempo t  (discretización temporal)
 	t = (double *) malloc(m * sizeof(double));
 	
 	t[0] = 0;
-	t[m] = tmax;
-	for( i = 1; i < m; i++ ) {
+	t[m-1] = tmax;
+	for( i = 1; i < m-1; i++ ) {
       	t[i] = t[i-1] + k ; 
+		printf("%f",t[i]);
 	}
 	
-	//Generando la matriz de coeficientes 
+	//Generando la matriz de coeficientes (1,...,n/1,....,n)
 	double **matrix = GeneraMatriz(m,n);
-	for (i=0; i<n; i++){
+	for (i=0; i<m; i++){
 		for (j=0; j<n; j++){
 			if (i==j){
 				matrix[i][j]=(1+2*lambda);
 				}
-			else if (j=i+1){
+			else if (j==i+1){
 				matrix[i][j]=-lambda;
 				}
-			else if(j=i-1){
+			else if(j==i-1){
 				matrix[i][j]=lambda;
 				}
 			}
 	}
 	
-	//Generando la matriz para valores de la funcion
-	double **matrixq = GeneraMatriz(m+1,n);
-	for (i=0; i<n; i++){
+	//Generando la matriz para valores de la funcion (1,....,n/1,...,n)
+	double **matrixq = GeneraMatriz(m,n);
+	for (i=0; i<m; i++){
 		for (j=0; j<n; j++){
-			matrixq[i][j]=k*funcion(x[i],t[i]);
+			matrixq[i][j]=k*funcion(x[i],t[j]);
 			}
 	}
 	
-	double **m_result = GeneraMatriz(m+1,n); //Declarando la matriz de resultados
+	double **m_result = GeneraMatriz(m,n); //Declarando la matriz de resultados
 	
-	// Obteniendo el vector frontera y solución
-	for (i=0; i<m+1; i++){
-		frontera[i] = exp(x[i]);
-	}
+	
 	
 	for (j=0; j<n; j++){
-		m_result[0][j]=frontera[j]; 
+		if(j==0){
+			m_result[0][j]=0;
+		}
+		else if(j==n-1){
+			m_result[0][j]=0;
+		}
+		else{
+			m_result[0][j]=exp(x[j]); 	
+		}
 	}
-	
-	
+		
 	for (i=0; i<n; i++){
-		b[i] = exp(x[i]);
+		inicial[i] = 0.5;
 	}
+	
+	//for (i=1; i<n-1; 1++){
+	//	m_result[i][0]=0;
+	//	m_result[i][n]=0;
+	//}
 	
 	
 
@@ -84,17 +93,18 @@ int main(){
 	
 	double *suma;
 	suma=(double *) malloc(n * sizeof(double));
-	for (i=0; i<m; i++){
+	for (i=0; i<m-1; i++){
 		suma=SumarVectores(n,m_result[i],matrixq[i+1]);
-		m_result[i+1]=MetodoJacobi(n,matrix,suma,b,tolerancia);
+		m_result[i+1]=MetodoJacobi(n,matrix,suma,inicial,tolerancia,20);
 		free(suma);
 	}
 	
 	
 	for (i = 0; i < m; i++){
-		for (j=0; j<n; j++){
-			printf("%f %f %f", t[i], x[j],m_result[i][j]);
-			}
+		for (j = 0; j < m; j++){
+			printf("%f",m_result[i][j]);	
+		} 
+		printf("\n");	
 	}
 	
 	
@@ -112,6 +122,10 @@ int main(){
 	}
 	fclose(dataimplicito);
 	
+	
+	
+	free(m_result);
+	free(matrixq);
 }
 
 double funcion(double x, double t){
