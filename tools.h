6a359feaa45e6d2ea305d0be324157b1d5a899c6
jhismaci <61795813+jhismaci@users.noreply.github.com>
2021-11-17 12:03:04 -0500

    Update tools.h

--- tools.h
+++ tools.h
@@ -7,7 +7,7 @@
 
 
 double **GeneraMatriz(int fil, int col);  // generará un arreglo de tamaño fil*col (filas por columna)
-double *MetodoJacobi(int n, double **A, double *b, double *xi, double tolerancia,int niteraciones); // método de Jacobi
+double *MetodoJacobi(int n, double **A, double *b, double tolerancia,int niteraciones); // método de Jacobi
 double *ProductoMatrizVector(int n, double **mat, double *arr);
 double *SumarVectores(int n, double *vector1, double *vector2);
 
