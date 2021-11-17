f24e267cb61b4629647ce3cbe16143b662ddf788
jhismaci <61795813+jhismaci@users.noreply.github.com>
2021-11-17 16:44:36 -0500

    Update tools.h

--- tools.h
+++ tools.h
@@ -7,7 +7,6 @@
 
 
 double **GeneraMatriz(int fil, int col);  // Generará un arreglo de tamaño fil*col (filas por columna)
-double norma(int n, double *vector1,double *vector2);
 double *MetodoJacobi(int n, double **A, double *b, double tolerancia, int niteraciones); // Método de Jacobi
 double *SumarVectores(int n, double *vector1, double *vector2); //Suma vectores columna o fila (de la misma dimensión)
 
